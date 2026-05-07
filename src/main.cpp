309c48c4ea90b24ad244b02f36bbac4d557f09c4
San1estus <j.rodriguezisiordia@gmail.com>
2026-05-07 00:57:32 -0600

    RRT of a simple car

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,50 @@
-/*First RRT Implementation */
+/*RRT Implementation for a simple car*/
 
 #include "config.h"
 #include "shader_utils.h"
+#define min(a,b) ((a) < (b) ? (a) : (b))
 
+// Car dimensions
+float carLength = 0.1f;
+float carWidth = 2/3.0f * carLength;
+
+std::vector<float> getCarVertices(float cx, float cy, float theta, float w, float h) {
+    std::vector<std::pair<float, float>> localVertices = {
+        {w/2, h/2},
+        {w/2, -h/2},
+        {-w/2, -h/2},
+        {-w/2, h/2}
+    };
+    for (auto& vertex : localVertices) {
+        float x = vertex.first;
+        float y = vertex.second;
+        vertex.first = x * cos(theta) - y * sin(theta);
+        vertex.second = x * sin(theta) + y * cos(theta);
+    }
+    for (auto& vertex : localVertices) {
+        vertex.first += cx;
+        vertex.second += cy;
+    }
+    std::vector<float> vertices;
+    for (const auto& vertex : localVertices) {
+        vertices.push_back(vertex.first);
+        vertices.push_back(vertex.second);
+    }
+    return vertices;
+}
+
+unsigned int carIndices[] = {
+    0, 1, 2,
+    2, 3, 0
+};
+
+// Global variables for VAOs and VBOs
 GLuint TreeVAO, TreeVBO;
 GLuint PathVAO, PathVBO;
 GLuint pointsVAO, pointsVBO;
+GLuint carVAO, carVBO, carEBO;
+
+// Buffers management functions
 void setupBuffers() {
     glGenVertexArrays(1, &TreeVAO);
     glGenBuffers(1, &TreeVBO);
@@ -27,6 +66,17 @@ void setupBuffers() {
     glBindBuffer(GL_ARRAY_BUFFER, pointsVBO);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glGenVertexArrays(1, &carVAO);
+    glGenBuffers(1, &carVBO);
+    glGenBuffers(1, &carEBO);   
+    glBindVertexArray(carVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, carVBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, carEBO);
+
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(carIndices), carIndices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 }
 
 void updateTreeBuffers(const std::vector<float>& vertices) {
@@ -39,11 +89,19 @@ void updatePathBuffers(const std::vector<float>& vertices) {
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_DYNAMIC_DRAW);
 }
 
-
+// RRT Implementation
 struct Node {
-    float x, y;
+    float x, y, theta;
     Node* parent;
 };
+
+const float PI = acos(-1.0f);
+
+float distance(Node* a, Node* b) {
+    float angleDiff = fabs(a->theta - b->theta);
+    float angleDist = min(angleDiff, 2 * PI - angleDiff);
+    return sqrt((a->x - b->x) * (a->x - b->x) + (a->y - b->y) * (a->y - b->y) + angleDist * angleDist);
+}
 
 struct RRT{
     std::vector<std::unique_ptr<Node>> nodes;
@@ -52,48 +110,57 @@ struct RRT{
     std::uniform_real_distribution<float> distX;
     std::uniform_real_distribution<float> distY;
     std::uniform_real_distribution<float> distBias;
-    
+    std::uniform_int_distribution<int> sampleV{-1, 1};
+    std::uniform_real_distribution<float> samplePhi{-PI/4, PI/4};
+    std::uniform_real_distribution<float> sampleTheta{-PI, PI};
+
+    float distWheels = 0.5f; 
     void init(float minX, float maxX, float minY, float maxY){
-        Node* init = addNode(0.0f, 0.0f, NULL);
+        Node* init = addNode(0.0f, 0.0f, 0.0f, nullptr);
         rng = std::mt19937(std::random_device{}());
         distX = std::uniform_real_distribution<float>(minX, maxX);
         distY = std::uniform_real_distribution<float>(minY, maxY);
         distBias = std::uniform_real_distribution<float>(0.0f, 1.0f);
     }
 
-    Node* addNode(float x, float y, Node* parent) {
+    Node* addNode(float x, float y, float theta, Node* parent) {
         auto node = std::make_unique<Node>();
         node->x = x;
         node->y = y;
+        node->theta = theta;
         node->parent = parent;
         nodes.push_back(std::move(node));
         return nodes.back().get();
     }
 
-    Node* nearest(float x, float y) {
+    Node* nearest(float x, float y, float theta) {
         Node* nearestNode = nullptr;
+        Node tempNode = Node{x, y, theta, nullptr};
         float minDist = std::numeric_limits<float>::max();
         for (const auto& node : nodes) {
-            float dist = std::hypot(node->x - x, node->y - y);
+            float dist = distance(node.get(), &tempNode);
             if (dist < minDist) {
                 minDist = dist;
                 nearestNode = node.get();
             }
         }
+
         return nearestNode;
     }
 
-    Node* steer(Node* from, float toX, float toY, float stepSize) {
-        float dx = toX - from->x;
-        float dy = toY - from->y;
-        float dist = std::hypot(dx, dy);
-        if (dist < stepSize) {
-            return addNode(toX, toY, from);
-        } else {
-            float newX = from->x + (dx / dist) * stepSize;
-            float newY = from->y + (dy / dist) * stepSize;
-            return addNode(newX, newY, from);
+    Node* steer(Node* from, int numSteps, float stepSize) {
+        float newX = from->x;
+        float newY = from->y;
+        float newTheta = from->theta;
+        for(int i = 0; i < numSteps; ++i){
+            int v = sampleV(rng);
+            float phi = samplePhi(rng);
+            newX += v * stepSize * cos(newTheta) * cos(phi);
+            newY += v * stepSize * sin(newTheta) * cos(phi);
+            newTheta += v * stepSize * tan(phi) / distWheels;
         }
+        return addNode(newX, newY, newTheta, from);
+        
     }
 
     Node* build(float goalX, float goalY, float goalRadius, int maxIter, float stepSize, float goalBias) {
@@ -101,15 +168,17 @@ struct RRT{
         if(currIter < maxIter){
             float randX = distX(rng);
             float randY = distY(rng);
+            float randTheta = sampleTheta(rng);
             if(distBias(rng) < goalBias){
                 randX = goalX;
                 randY = goalY;
+                randTheta = atan2(goalY - nodes.back()->y, goalX - nodes.back()->x);
             }
-            Node* nearestNode = nearest(randX, randY);
+            Node* nearestNode = nearest(randX, randY, randTheta);
             
-            Node* newNode = steer(nearestNode, randX, randY, stepSize);
+            Node* newNode = steer(nearestNode, 10, stepSize);
             if(std::hypot(newNode->x - goalX, newNode->y - goalY) < goalRadius){
-                return addNode(goalX, goalY, newNode);
+                return addNode(goalX, goalY, atan2(goalY - newNode->y, goalX - newNode->x), newNode);
             } 
             currIter++;
         }
@@ -140,6 +209,8 @@ struct RRT{
         return vertices;
     }
 };
+
+
 int main() {
     GLFWwindow* window;
 
@@ -171,18 +242,24 @@ int main() {
     std::vector<Node*> path;
     std::vector<float> pathVertices;
     std::vector<float> initAndGoalVertices = {0.0f, 0.0f, 0.8f, 0.8f};
-    
+    std::vector<float> carVertices;
+    int pathIndex = 0;
     bool pathFound = false;
     setupBuffers();
+    glBindBuffer(GL_ARRAY_BUFFER, pointsVBO); 
     glBufferData(GL_ARRAY_BUFFER, initAndGoalVertices.size() * sizeof(float), initAndGoalVertices.data(), GL_STATIC_DRAW);
     glPointSize(10.0f);
+    float deltaTime = 0.0f;
+    float lastFrame = 0.0f;
     while(!glfwWindowShouldClose(window)) {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
         
         std::vector<float> edgeVertices;
             if(!goalNode){
-                goalNode = rrt.build(initAndGoalVertices[2], initAndGoalVertices[3], 0.05f, 1000, 0.05f, 0.1f);
+                goalNode = rrt.build(initAndGoalVertices[2], initAndGoalVertices[3], 0.1f, 10000, 0.02f, 0.1f);
                 if(goalNode){
                     path = rrt.getPath(goalNode);
                 }
@@ -210,6 +287,19 @@ int main() {
             }
             glBindVertexArray(PathVAO);
             glDrawArrays(GL_LINES, 0, pathVertices.size() / 2);
+
+            if(pathIndex < path.size()){
+                if(deltaTime >= 0.1f){
+                    carVertices = getCarVertices(path[pathIndex]->x, path[pathIndex]->y, path[pathIndex]->theta, carLength, carWidth);
+                    pathIndex++;
+                    lastFrame = currentFrame;
+                } 
+                glBindBuffer(GL_ARRAY_BUFFER, carVBO);
+                glBufferData(GL_ARRAY_BUFFER, carVertices.size() * sizeof(float), carVertices.data(), GL_DYNAMIC_DRAW);
+                glUniform4f(colorLoc, 0.0f, 0.0f, 1.0f, 1.0f);
+                glBindVertexArray(carVAO);
+                glDrawElements(GL_TRIANGLES, sizeof(carIndices) / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+            }
         }
         
         glUniform4f(colorLoc, 1.0f, 1.0f, 0.1f, 1.0f);
